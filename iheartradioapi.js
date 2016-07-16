0403d8e16406d774851437b0285a04595869a9d4
James Lim <james.albert.lim@gmail.com>
2016-07-16 07:56:07 -0700

    browse stations by city working

--- iheartradioapi.js
+++ iheartradioapi.js
@@ -1,44 +1,45 @@
 'use strict';
-
+var express = require('express');
+var router = express.Router();
+var Promise = require("bluebird");
 require("dotenv").load();
 var request = require('request');
 
 var apiBaseUrl = 'https://us.api.iheart.com/api';
 var liveStationBrowseUrl = '/api/v2/content/liveStations';
 
+var countryCode = 'US';
+var resultsLimit = 10000;
 var browseMarketUrl = '/v2/content/markets';
+var url = `${apiBaseUrl}${browseMarketUrl}?${countryCode}&limit=${resultsLimit}`;
 
-function topTwentyCities() {
-  var countryCode = 'US';
-  var resultsLimit = 10000;
-  var url = `${apiBaseUrl}${browseMarketUrl}?${countryCode}&limit=${resultsLimit}`;
-  var topTwentyCitiesSeed = ['New York',
-                         'Los Angeles',
-                         'Chicago',
-                         'Houston',
-                         'Philadelphia',
-                         'Phoenix',
-                         'San Antonio',
-                         'San Diego',
-                         'Dallas',
-                         'San Jose',
-                         'Austin',
-                         'Jacksonville',
-                         'San Francisco',
-                         'Indianapolis',
-                         'Coulmbus',
-                         'Fort Worth',
-                         'Charlotte',
-                         'Seattle',
-                         'Denver',
-                         'El Paso'];
-  var topTwentyCitiesArr = [];
-
-  request.get(url, function(err, res, body) {
-    body = JSON.parse(body);
-    for (var i = 0; i < body.hits.length; i++) {
-      if (topTwentyCitiesSeed.indexOf(body.hits[i].city) !== -1) {
-        topTwentyCitiesArr.push(body.hits[i]);
+router.get('/', function(req, res, next) {
+    var topTwentyCitiesSeed = ['New York',
+    'Los Angeles',
+    'Chicago',
+    'Houston',
+    'Philadelphia',
+    'Phoenix',
+    'San Antonio',
+    'San Diego',
+    'Dallas',
+    'San Jose',
+    'Austin',
+    'Jacksonville',
+    'San Francisco',
+    'Indianapolis',
+    'Coulmbus',
+    'Fort Worth',
+    'Charlotte',
+    'Seattle',
+    'Denver',
+    'El Paso'];
+    var topTwentyCitiesArr = [];
+    request(url, function(err, data) {
+    var dataArray = JSON.parse(data.body);
+    for (var i = 0; i < dataArray.hits.length; i++) {
+      if (topTwentyCitiesSeed.indexOf(dataArray.hits[i].city) !== -1) {
+        topTwentyCitiesArr.push(dataArray.hits[i]);
       }
     }
     var citiesSorted = topTwentyCitiesArr.sort(function(a, b) {
@@ -46,9 +47,9 @@ function topTwentyCities() {
       if (a.city > b.city) return 1;
       return 0;
     });
-    return citiesSorted;
+    res.render('browse', {cities:topTwentyCitiesArr});
   });
-}
+});
 
 function browseGenres() {
   var liveStationGenreUrl = '/v2/content/liveStationGenres';
@@ -62,7 +63,4 @@ function browseGenres() {
   });
 }
 
-module.exports = {
-  topTwentyCities : topTwentyCities,
-  browseGenres : browseGenres
-};
+module.exports = router;
